5afa3e2793a6badb2849c03807ab82153622624b
David Chu <dabidjohn424@gmail.com>
2026-02-21 20:56:53 +0800

    refactor: add line of code, update style layouts and properties

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import type { GroceryItem } from "./types/index";
 import { useStore } from "./store/store";
 import { useState } from "react";
 import Form from "./components/Form";
+import Button from "./components/Button";
+import bblogo from "./assets/budget-buddy-logo.svg";
 
 const App = () => {
   //STATES
@@ -22,7 +24,7 @@ const App = () => {
     quantity: 1,
   });
 
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const name = e.target.name;
@@ -39,27 +41,47 @@ const App = () => {
   };
 
   const updateBudget = items.reduce(
-    (T, item) => T + item.price * item.quantity,
+    (t, item) => t + item.price * item.quantity,
+    0,
+  );
+
+  const totalPriceProducts = items.reduce(
+    (t, item) => t + item.price * item.quantity,
     0,
   );
 
   const subtractBudget = budget - updateBudget;
 
   return (
-    <div className="flex justify-center flex-col items-center">
-      <h1>Budget Buddy </h1>
-      <div className="border-b-2 w-full max-w-70">
-        <input
-          className="outline-none text-3xl"
-          type="text"
-          value={budget}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setBudget(Number(e.target.value))
-          }
-        />
+    <div className="flex justify-center flex-col items-center itim-regular p-2 max-w-300 mx-auto">
+      <img src={bblogo} alt="" />
+      <div className="flex items-end mt-10 w-full">
+        <p className="text-3xl sm:text-7xl">Budget:</p>
+        <div className="border-b-2 w-full border-[#F39B9A]">
+          <input
+            className="outline-none text-2xl sm:text-5xl pl-5 w-full"
+            type="text"
+            value={budget}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setBudget(Number(e.target.value))
+            }
+          />
+        </div>
       </div>
-      <p>Input your budget above</p>
-      {budget && subtractBudget}
+
+      <div className="flex justify-start w-full mt-13">
+        <Button />
+      </div>
+
+      <div className="text-center mt-15">
+        <p className="text-lg sm:text-2xl">Budget {`(-)`} minus Products</p>
+        <p className="text-5xl sm:text-7xl text-[#F39B9A]">
+          {budget && subtractBudget}
+        </p>
+        <p className="text-2xl">
+          {budget} - {totalPriceProducts}
+        </p>
+      </div>
 
       <div>
         {isOpen && (
@@ -71,34 +93,37 @@ const App = () => {
         )}
       </div>
 
-      <div className="w-full">
-        <table className="w-full text-start">
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Actions</th>
-          </tr>
-          {items.map((item) => {
-            return (
-              <tr className="text-center">
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.quantity}</td>
-                <td>
-                  {
-                    <button
-                      className="bg-amber-300"
-                      onClick={() => removeCartItem(item.id)}
-                    >
-                      remove
-                    </button>
-                  }
-                </td>
-              </tr>
-            );
-          })}
-        </table>
+      <div className="w-full mt-15 ">
+        <p className="mb-2 sm: text-xl">Products Table</p>
+        <div className="w-full border rounded">
+          <table className="w-full text-start">
+            <tr className="border-b">
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Actions</th>
+            </tr>
+            {items.map((item) => {
+              return (
+                <tr className="text-center">
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    {
+                      <button
+                        className="bg-amber-300"
+                        onClick={() => removeCartItem(item.id)}
+                      >
+                        remove
+                      </button>
+                    }
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+const Button = () => {
+  return (
+    <button className="bg-white rounded-[5px] px-2.5 pt-2.5 shadow-[5px_4px_0px] shadow-gray-400 border border-[#F39B9A]">
+      <p className="text-lg">
+        <span className="font-bold text-2xl">+</span> Add Products
+      </p>
+    </button>
+  );
+};
+
+export default Button;
